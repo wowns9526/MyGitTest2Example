446ba753f8c38a5d2186f16d08fb61be6e3d0129
wowns9526 <wowns9526@naver.com>
2021-03-18 13:22:46 +0900

    activity_main + id

--- app/src/main/java/com/example/mygittest2example/MainActivity.java
+++ app/src/main/java/com/example/mygittest2example/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.mygittest2example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = findViewById(R.id.tv1);
     }
 }
